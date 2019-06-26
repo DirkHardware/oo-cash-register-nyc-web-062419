701c2af2f1ecbdaeaa06b7a6d4bf84299fd006ae
Anderson Rolf <email.the.anderson@gmail.com>
2019-06-26 00:49:59 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -16,4 +16,9 @@ class CashRegister
   def add_item(title, price, quantity = 1)
     @total += price * quantity
   end
+
+  def apply_discount
+    if @employee_discount > 0
+      @total -= @employee_discount
+      "After the discount, the total comes to #{@total}."
 end
